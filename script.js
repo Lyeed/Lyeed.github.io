2b802bcc227cf0f2acf1a80f6d37857444b87b2b
LyeedoO <gautier.jousset@epitech.eu>
2018-07-23 19:16:49 +0200

    temp version

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function swapDiv(div, title)
-{
-   document.getElementById('perso').style.display = "none";
-   document.getElementById('diplomes').style.display = "none";
-   document.getElementById('competences').style.display = "none";
-   document.getElementById('projets').style.display = "none";
-
-   document.getElementById(div).style.display = "block";
-   document.getElementById("subtitle").innerHTML = title;
+function copyTextToClipboard(element) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val('gautier.jousset@epitech.eu').select();
+	document.execCommand("copy");
+	$temp.remove();
+	alert("Copied into clipboard");
 }
