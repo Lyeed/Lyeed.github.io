2130724671bdb3ab9bd24802ef1074ed92f9b752
Lyeed <gautier.jousset@epitech.eu>
2020-04-04 23:31:37 +0200

    Add some information and links

--- script.js
+++
@@ -1,8 +0,0 @@
-function copyTextToClipboard(element) {
-	var $temp = $("<input>");
-	$("body").append($temp);
-	$temp.val('gautier.jousset@epitech.eu').select();
-	document.execCommand("copy");
-	$temp.remove();
-	alert("Copied into clipboard");
-}
